600c1d492f119b8b67b71add0346dd0d314a43c6
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2025-01-22 22:02:44 +0100

    repository

--- app/src/main/java/com/carlosdev/randomusersapp/data/repository/RandomUsersRepository.kt
+++ app/src/main/java/com/carlosdev/randomusersapp/data/repository/RandomUsersRepository.kt
@@ -5,7 +5,10 @@ import com.carlosdev.randomusersapp.data.model.User
 import com.carlosdev.randomusersapp.data.remote.IRandomUserApi
 import com.carlosdev.randomusersapp.domain.repository.IRandomUsersRepository
 
-class RandomUsersRepository(private val randomUserApi: IRandomUserApi, private val realmDatabase: RealmDatabase) : IRandomUsersRepository {
+class RandomUsersRepository(
+    private val randomUserApi: IRandomUserApi,
+    private val realmDatabase: RealmDatabase
+) : IRandomUsersRepository {
     override suspend fun getUsersFromApi(): List<User> {
         val users = randomUserApi.getUsers().results
         saveUsers(users)
@@ -16,9 +19,15 @@ class RandomUsersRepository(private val randomUserApi: IRandomUserApi, private v
         if (realmDatabase.getUsers().isEmpty()) {
             getUsersFromApi()
             return realmDatabase.getUsers()
-        }else{
+        } else {
             return realmDatabase.getUsers()
         }
+    }
+
+    override suspend fun getNewUsers(): List<User> {
+        val newUsers = getUsersFromApi()
+        saveUsers(newUsers)
+        return getUsersFromDatabase()
     }
 
     override suspend fun saveUsers(users: List<User>) {
@@ -27,5 +36,14 @@ class RandomUsersRepository(private val randomUserApi: IRandomUserApi, private v
 
     override suspend fun deleteUser(user: User) {
         realmDatabase.markUserAsDeleted(user)
+    }
+
+    override suspend fun getUserById(id: String): User {
+        val user = realmDatabase.getUserById(id)
+        if (user != null) {
+            return user
+        } else {
+            throw Exception("User not found")
+        }
     }
 }
--- app/src/main/java/com/carlosdev/randomusersapp/domain/repository/IRandomUsersRepository.kt
+++ app/src/main/java/com/carlosdev/randomusersapp/domain/repository/IRandomUsersRepository.kt
@@ -6,6 +6,8 @@ interface IRandomUsersRepository {
 
     suspend fun getUsersFromApi(): List<User>
     suspend fun getUsersFromDatabase(): List<User>
+    suspend fun getNewUsers(): List<User>
     suspend fun saveUsers(users: List<User>)
     suspend fun deleteUser(user: User)
+    suspend fun getUserById(id: String): User
 }
