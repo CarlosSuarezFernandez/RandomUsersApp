f001f059c3f9db5bc2577bd17b3631b8e560928d
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2025-01-22 22:03:14 +0100

    data model

--- app/src/main/java/com/carlosdev/randomusersapp/data/model/RandomUserResponse.kt
+++ app/src/main/java/com/carlosdev/randomusersapp/data/model/RandomUserResponse.kt
@@ -1,5 +1,9 @@
 package com.carlosdev.randomusersapp.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RandomUserResponse(
-    val results: List<User>
+    val results: List<User>,
+    val info: Info
 )
